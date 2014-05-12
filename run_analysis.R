355c43adcba7274a71b60de91bf6a88f72b705e8
Claudio Caccia <c.caccia@libero.it>
2014-05-12 15:30:22 +0200

    updated .gitignore - defined file paths

--- run_analysis.R
+++ run_analysis.R
@@ -1,3 +1,22 @@
+# packages
+#package(data.table)
 # read dataset
 
-dataDirName = "./UCI HAR Dataset"
+
+# directories
+
+data.dir = "./UCI HAR Dataset"
+
+train.dir = "/train"
+test.dir = "/test"
+
+# files
+
+X.test.DataFile = "/X_test.txt"
+y.test.DataFile = "/y_test.txt"
+
+X.train.DataFile = "/X_train.txt"
+y.train.DataFile = "/y_train.txt"
+
+Xtest<-read.table(paste(c(data.dir, test.dir, X.test.DataFile), collapse=''))
+#Xtr<-read.table(paste(c(data.dir, train.dir, X.train.DataFile), collapse=''), sep=" " )
