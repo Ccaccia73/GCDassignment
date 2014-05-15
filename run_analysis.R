9d087795c3c0986f9488fc75b796c5576053ad40
Claudio Caccia <c.caccia@libero.it>
2014-05-15 14:16:57 +0200

    read files and built main table

--- run_analysis.R
+++ run_analysis.R
@@ -18,7 +18,36 @@ y.test.DataFile = "/y_test.txt"
 X.train.DataFile = "/X_train.txt"
 y.train.DataFile = "/y_train.txt"
 
+train.SubjectFile = "/subject_train.txt"
+test.SubjectFile = "/subject_test.txt"
+
+features.DataFile = "/features.txt"
+
+# Part 1
+
 Xtrain<-read.table(paste(c(data.dir, train.dir, X.train.DataFile), collapse=''))
 Xtest<-read.table(paste(c(data.dir, test.dir, X.test.DataFile), collapse=''))
 
 Xall<-rbind(Xtrain,Xtest)
+
+Ytrain<-read.table(paste(c(data.dir, train.dir, y.train.DataFile), collapse=''))
+Ytest<-read.table(paste(c(data.dir, test.dir, y.test.DataFile), collapse=''))
+
+Activity.ID <- rbind(Ytrain, Ytest)
+
+Subj.train<-read.table(paste(c(data.dir, train.dir, train.SubjectFile), collapse=''))
+Subj.test<-read.table(paste(c(data.dir, test.dir, test.SubjectFile), collapse=''))
+
+Subj.all<-rbind(Subj.train,Subj.test)
+
+# Part 2
+
+features.ID <- read.table(paste(c(data.dir, features.DataFile), collapse=''))
+meanStd.ID <- subset(features.ID,grepl("mean|std", features.ID[ ,2]))
+
+MeanStd.Data <- Xall[,meanStd.ID[ ,1] ]
+
+Data.all<-cbind(MeanStd.Data,Activity.ID,Subj.all)
+
+
+
